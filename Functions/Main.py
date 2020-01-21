65a9fb0bb3343b0ef44dd37a6b2b9334aba43bb8
kworrell kworrell <chemicalcoal1@gmail.com>
2020-01-20 22:35:12 -0700

    added comments to Main.py

--- Functions/Main.py
+++ Functions/Main.py
@@ -12,47 +12,44 @@ import datetime
 import statistics
 import json
 
-Discount = .7
+Discount = .7  # Minimum percent of original price, avg of 10$ item will need to be below 7$ if .7 is used
+SpecificProductDiscount = 2  # Percent of origional price for items in specific item section
 
-SearchEverything = 0
-ReuseSearch = 0
-StoppingPrice = [50,6.0]
-MaxCalls = 0
+SearchEverything = 0  # 0 If searching only auctions 1 if searching all listings
+ReuseSearch = 0  # Use data from previous search instead of generating new search items
+StoppingPrice = [50,6.0]  # [Auctions Only, Everything] Max price without shipping
+MaxCalls = 0  # Maximum calls per item search
 
-StoppingPrice = StoppingPrice[SearchEverything]
+EndingSoon = int(60*60*24*2.2)  # Unix time cutoff for item to be considered ending soon
 
-EndingSoon = int(60*60*24*2.2)
+StoppingPrice = StoppingPrice[SearchEverything]  # Redifine Stopping Price with Search Everything
 
-if(SearchEverything == 1):
+if(SearchEverything == 1):  # If Searching everything then no limit on items ending soon since most items are buy now
     EndingSoon = int(60*60*24*32)
 
+# Grab search list from Searching file
 SearchingFile = str(os.path.dirname(os.path.dirname(__file__))) + '/DataBase/Searching'
 with open(SearchingFile, 'r') as in_file:
     SearchingFileData = json.load(in_file)
-
 Searching = SearchingFileData["Searching"]
-
-SpecificProductDiscount = 2
 SpecificProductSearch = SearchingFileData["SpecificProductSearch"]
 
-if('dvd' in Searching):
-    StoppingPrice = .1
-    print('Set price to .1 for search')
-
+#Inilitization of Global Variables
 Output = []
 OutputSpecific = []
 Errors = []
 Lot = []
-
 Display = []
+
+
+# AddToGUI adds items to the Display global variable in the correct format for the GUI to display later
 def AddToGUI(Name,SearchName,Price,AvgPrice,ImageURL,PageURL,ComparisonData):
     #Comparison Data in format [[SearchURL1,SearchImage1,Name1,Price1],[SearchURL2,SearchImage2,Name2,Price2],...]
     global Display
-    print([Name,SearchName,Price,AvgPrice,ImageURL,PageURL,ComparisonData])
-    print(len(Display))
-    print(Display)
     Display.append([Name,SearchName,Price,AvgPrice,ImageURL,PageURL,ComparisonData])
 
+
+# Total Search searches everything in the Searching Title variable
 def TotalSearch(SearchingTitle,SearchNonAuctions):
     global Output
     global OutputSpecific
@@ -61,18 +58,19 @@ def TotalSearch(SearchingTitle,SearchNonAuctions):
                       os.listdir(str(os.path.dirname(os.path.dirname(__file__))) + '/DataBase/Searches/')]))
     dbSearches = TinyDB(os.path.dirname(os.path.dirname(__file__)) + "/DataBase/Searches/" + DBName)
 
-    for SearchingNonAuctions in range(SearchNonAuctions+1):
+    for SearchingNonAuctions in range(SearchNonAuctions+1): # Runs once if only auctions and twice if everything
 
-        if SearchingNonAuctions == 0:
-            AuctionSearch = 1
+        if SearchingNonAuctions == 0:  # If Searching auctions
+            AuctionSearch = 1  # Searching Auctions
         else:
             AuctionSearch = 0
 
-        SearchList = Search.GenerateSearch(SearchingTitle, StoppingPrice, AuctionSearch)
+        SearchList = Search.GenerateSearch(SearchingTitle, StoppingPrice, AuctionSearch)  # Generate search array
 
-        Search.Search(SearchingTitle, AuctionSearch, SearchList,dbSearches)
+        Search.Search(SearchingTitle, AuctionSearch, SearchList,dbSearches)  # Search items in search array
 
 
+# Analisis evaluates all items in Items List to see if they are deals or not
 def Analisis(ItemsList):
     OutOfCalls = 0
     global MaxCalls
@@ -82,38 +80,38 @@ def Analisis(ItemsList):
     ShippingDataBase = TinyDB(os.path.dirname(os.path.dirname(__file__)) + "/DataBase/LinkToShipping")
     AvgPriceDataBase = TinyDB(os.path.dirname(os.path.dirname(__file__)) + "/DataBase/LinkToAvgPrice")
     ErrorsDataBase = TinyDB(os.path.dirname(os.path.dirname(__file__)) + "/Logs/Errors")
-
     WebDriverPath = os.path.dirname(os.path.dirname(__file__)) + '/Drivers/chromedriver.exe'
 
-    TotalCount = len(ItemsList)
+    TotalCount = len(ItemsList)  # Total Count of items to search
 
-    if(MaxCalls == 0):
+    if(MaxCalls == 0):  # No maximum call count
         CallCount = len(ItemsList)
-    elif MaxCalls > len(ItemsList):
+    elif MaxCalls > len(ItemsList):  # More calls than items
         CallCount = len(ItemsList)
-    else:
+    else:  # Call count is limited by Maximum calls
         CallCount = MaxCalls
 
-    LastCycle = time.time()
-    RecordedCycleTime = []
-    for Item in range(CallCount):
-        CycleTime = time.time() - LastCycle
-        RecordedCycleTime.append(CycleTime)
+    LastCycle = time.time()*2  # Record cycle time for time predictions
+    RecordedCycleTime = []  # Initilize empty time per cycle array
+    for Item in range(CallCount):  # For every item
+        CycleTime = time.time() - LastCycle  # Calculate time it took for last item to be analized
+        RecordedCycleTime.append(CycleTime)  # Keep track of number
 
-        if(len(RecordedCycleTime) > 300):
+        # If more than 300 numbers remove the first number in the list, or if the first number is negative and there is more than one number
+        if(len(RecordedCycleTime) > 300 or (RecordedCycleTime[0] < 0 and len(RecordedCycleTime >= 2))):
             RecordedCycleTime = RecordedCycleTime[1:len(RecordedCycleTime)]
 
-
+        # Calculate expected time remaining in mins
         ExpectedTimeRemaining = statistics.mean(RecordedCycleTime)*(TotalCount-Item)/60
-        LastCycle = time.time()
+        LastCycle = time.time()  # Record start time for analysis
 
+        # Get Item information
         ItemSearchKeywords = ItemsList[Item]['SearchKeywords']
         ItemURL = ItemsList[Item]['viewItemURL'][0]
         ItemTitle = ItemsList[Item]['title'][0]
         ItemUPC = Get.UPC(ItemURL,UPCDataBase)
         ItemPrice = ItemsList[Item]['sellingStatus'][0]['convertedCurrentPrice'][0]['__value__']
         ItemShippingType = ItemsList[Item]['shippingInfo'][0]['shippingType'][0]
-        print(ItemsList[Item])
         try:
             ItemPicture = ItemsList[Item]['pictureURLSuperSize'][0]
         except:
@@ -123,10 +121,12 @@ def Analisis(ItemsList):
                 ItemPicture = ""
         ItemName = ItemsList[Item]['title'][0]
 
+        #Display stuff
         sys.stdout.write(
             "\rSearching %d" % Item + " of %d" % TotalCount + " " + "Minutes Remainging: %.2f" % ExpectedTimeRemaining + " " + str(ItemTitle) + " ")
         sys.stdout.flush()
 
+        #Calculating Endtime of listing
         ItemEndTime = ItemsList[Item]['listingInfo'][0]['endTime'][0]
         Year = int(ItemEndTime[0:4])
         Month = int(ItemEndTime[5:7])
@@ -136,10 +136,10 @@ def Analisis(ItemsList):
         dt = datetime.datetime(Year, Month, Dayy, Hour, Minute, 0)
         UnixEndingStamp = time.mktime(dt.timetuple())
 
-        if(float(UnixEndingStamp) < time.time()):
-
+        if(float(UnixEndingStamp) < time.time()):  # Item auction end time has passed
             print('Auction ended already for: ' + str(ItemURL))
 
+        # Get shipping on items where shipping changes for different places
         if ItemShippingType == 'Calculated' or ItemShippingType == 'CalculatedDomesticFlatInternational' or ItemShippingType == 'FreePickup':
             ItemShipping,driver = Get.Shipping(ItemURL,driver,ShippingDataBase,WebDriverPath)
         else:
@@ -149,21 +149,18 @@ def Analisis(ItemsList):
                 continue
 
         global Lot
-        if('lot' in ItemTitle.lower()):
-
+        if('lot' in ItemTitle.lower()):  # If item is clearly a multi item auction
             Info = [float(ItemPrice) + float(ItemShipping), str(ItemURL),str(False)]
 
             if(Info not in Lot and UnixEndingStamp < time.time()+EndingSoon):
-                Lot.append(Info)
+                Lot.append(Info)  # Store item info in Lot variable and in GUI
                 Logs.Write("Found Lot Item: " + str(Info))
                 AddToGUI(ItemName, '', str(float(ItemPrice)+float(ItemShipping)), '0', ItemPicture, ItemURL, [])
             continue
-        else:
+        else:  # Item is probably a single item
             Prices,FinalSearchName,SearchedItems = Get.AvgPrice(ItemURL,ItemTitle,OutOfCalls,float(ItemPrice) + float(ItemShipping),ItemSearchKeywords,float(UnixEndingStamp),AvgPriceDataBase,ErrorsDataBase,UPCDataBase)
 
-        print(Prices)
-
-        if(FinalSearchName == None or ItemTitle == None):
+        if(FinalSearchName == None or ItemTitle == None):  # Issues getting specific item names
             print("Error: 24, None type")
             print(FinalSearchName)
             print(ItemTitle)
@@ -171,6 +168,7 @@ def Analisis(ItemsList):
             Logs.Write("Error: 24, None type " + str(FinalSearchName) + " " + str(ItemTitle) + " " + str(ItemURL) + " " + str(ItemPrice) + " " + str(ItemShipping))
             continue
 
+        # If multi item auction predicted
         if(('collection' in ItemTitle.lower() or 'collection' in FinalSearchName or 'Multi Item Auction Found' == FinalSearchName) and Prices <= 0):
             Info = [float(ItemPrice) + float(ItemShipping), str(ItemURL), str('Multi Item Auction Found' == FinalSearchName)]
 
@@ -184,17 +182,18 @@ def Analisis(ItemsList):
             OutOfCalls = 1
             continue
 
-        if Prices == -1 or Prices == None:
+        if Prices == -1 or Prices == None:  # Error finding avg price
             continue
 
-        if ItemSearchKeywords in SpecificProductSearch:
+        if ItemSearchKeywords in SpecificProductSearch:  # If item needs to be evaluated by Specific Product Search
             global SpecificProductDiscount
-            SearchingForDiscount = SpecificProductDiscount
+            SearchingForDiscount = SpecificProductDiscount  # Use Specific Product discount
+            # If ending soon, and within discount range
             if (float(Prices) * SearchingForDiscount > float(ItemPrice) + float(ItemShipping)) and UnixEndingStamp < time.time()+EndingSoon and ItemURL not in [ItemInfo[2] for ItemInfo in OutputSpecific]:
                 OutputSpecific.append([float(ItemPrice) + float(ItemShipping) , float(Prices) , str(ItemURL), str(FinalSearchName), str(SearchingForDiscount), str(ItemSearchKeywords)])
                 AddToGUI(ItemName, FinalSearchName, str(float(ItemPrice)+float(ItemShipping)), Prices, ItemPicture, ItemURL, SearchedItems)
                 Logs.Write("Found Discounted Specific Search Item" + str([float(ItemPrice) + float(ItemShipping) , float(Prices) , str(ItemURL), str(FinalSearchName), str(ItemSearchKeywords)]))
-        else:
+        else:  # Use normal discount
             SearchingForDiscount = Discount
 
             if (float(Prices) * SearchingForDiscount > float(ItemPrice) + float(ItemShipping)) and UnixEndingStamp < time.time()+EndingSoon and ItemURL not in [ItemInfo[2] for ItemInfo in Output]:
@@ -203,23 +202,24 @@ def Analisis(ItemsList):
                 Logs.Write("Found Discounted Item" + str([float(ItemPrice) + float(ItemShipping) , float(Prices) , str(ItemURL), str(FinalSearchName), str(ItemSearchKeywords)]))
 
     try:
-        driver.Quit()
+        driver.Quit()  # Close driver after complete
     except:
         print('drive.Quit Failed')
 
 if __name__ == "__main__":
 
+    # New logs and ebay directory
     Logs.New()
     Ebay.New()
 
     dbErrors = TinyDB(os.path.dirname(os.path.dirname(__file__)) + "/Logs/Errors")
-    dbErrors.purge_tables()
+    dbErrors.purge_tables()  # Reset Errors Data Base
 
-    SearchText = Searching
-    for i in SpecificProductSearch:
+    SearchText = Searching  # Data to search
+    for i in SpecificProductSearch:  # Add all the Specific product search stuff to Seart Text
         SearchText.append(i)
 
-    if(ReuseSearch == 0):
+    if(ReuseSearch == 0):  # If getting new search items
         Search.New()
         for SearchingTitleText in SearchText:
             TotalSearch(SearchingTitleText,SearchEverything)
@@ -229,60 +229,57 @@ if __name__ == "__main__":
     dbSearches = TinyDB(os.path.dirname(os.path.dirname(__file__)) + "/DataBase/Searches/" + DBName)
     Data = dbSearches.table("_default").all()
 
-    Analisis(Data)
+    Analisis(Data)  # Analise data
 
-    Output.sort()
+    Output.sort()  # Sort based on price
     OutputSpecific.sort()
 
+    # Get all the errors into one variable
     dbErrors = TinyDB(os.path.dirname(os.path.dirname(__file__)) + "/Logs/Errors")
-
     dbErrorsTable = dbErrors.table().all()
-
     for a in range(len(dbErrorsTable)):
         try:
             dbErrorsTable[a]['Price']
         except:
             print(dbErrorsTable[a])
 
-    for a in range(len(dbErrorsTable)):
         if(dbErrorsTable[a]["EndTime"] < time.time()+EndingSoon and dbErrorsTable[a]["EndTime"] > time.time()):# + 60*60*17):
             print(dbErrorsTable[a]["EndTime"])
             Errors.append([dbErrorsTable[a]['Price'],str(dbErrorsTable[a]['Error']) + " " + str(dbErrorsTable[a]['ItemLink']) + " " + str([dbErrorsTable[a]['ItemTitle']]) + " " + str([dbErrorsTable[a]['CallText']]) + str(dbErrorsTable[a]["EndTime"]) + " | " + str(dbErrorsTable[a]['SearchKeywords'])])
 
-    for i in OutputSpecific:
+    for i in OutputSpecific:  # add the Specific items to the main output variable
         Output.append(i)
 
+    # Remove all non ascii characters from Output
     Output = [" ".join([str(word) for word in a]).encode('ascii','ignore').decode() for a in Output]
 
-    Output.append("")
+    Output.append("")  # Add blank line between Single items and lot items
 
-    Lot.sort()
-    print(Lot)
+    Lot.sort()  # add lot items to main output variable
     for i in range(len(Lot)):
         Output.append(str(Lot[i][0]) + " " + str(Lot[i][1]) + " " + str(Lot[i][2]))
 
-    Output.append("")
+    Output.append("")  # Add line between lot items and errors
 
-    Errors.sort()
-
+    Errors.sort()  # Sort errors and reformat variable
     ErrorsCombined = [str(E[0]) + " " + str(E[1]) for E in Errors]
     Errors = ErrorsCombined
 
-    for i in Errors:
+    for i in Errors:  # remove non ascii characters and add errors to main output variable
         RemovedAscii = i.encode('ascii','ignore').decode()
         if RemovedAscii.split("|")[0] not in [Output[a].split("|")[0] for a in range(len(Output))]:
             Output.append(RemovedAscii)
 
+    # Put headers at the beginning of output variable
     Output.insert(0, str(len(Errors)) + " Errors out of " + str(len(Data)))
     Output.insert(1,"")
 
-    Mail.Send(Output)
+    Mail.Send(Output)  # Send Output as mail
 
-    print(Display)
-
+    # Save Output to file
     DisplayDataPosition = str(os.path.dirname(os.path.dirname(__file__))) + '/DataBase/DisplayData.txt'
     with open(DisplayDataPosition, 'w') as out_file:
         json.dump(Display, out_file)
 
     print("Starting Display")
-    Disp.Startup(Display)
+    Disp.Startup(Display)  # Start GUI
