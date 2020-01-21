aabf8a2f9fd71a41dc6b8306c14d14f5d78c94d5
kworrell kworrell <chemicalcoal1@gmail.com>
2020-01-20 20:03:17 -0700

    Reduced reused code

--- Functions/Ebay.py
+++ Functions/Ebay.py
@@ -7,63 +7,91 @@ from tinydb import TinyDB, Query
 from datetime import datetime
 from tinydb.operations import increment
 
+########################################################################################################################
+# Ebay.py
+#
+# Ebay contains two functions: New and Call
+#
+# New() creates a new folder for all of the call data to be placed in under Logs/Calls
+# Call("Keywords",(int 1/0)AuctionOnly,(int 1/100)Page,(float)MaxPrice,(float)MinPrice,(int 1/0)Present)
+# Call returns the json data structure from ebay containing item information. If Auction Only is 1 it will only search
+# auctions, if it is 0 it will search everything, similarly if Present is 1 it will search only current items and if it
+# is 0 it will search all completed items. All items returned will lie within Min and Max Price numbers
+#
+########################################################################################################################
+
+# Initilization
+
+# Get Ebay Api Key from PasswordsAndSuch File
 PasswordsFile = str(os.path.dirname(os.path.dirname(__file__))) + '/DataBase/PasswordsAndSuch'
 with open(PasswordsFile, 'r') as in_file:
     Passwords = json.load(in_file)
-
 SecurityAppName = Passwords['SecurityAppName']
 
+try:
+    CallFolder = str(max([float(s.replace('.txt', '')) for s in os.listdir(str(os.path.dirname(os.path.dirname(__file__))) + '/LOGS/Calls/')]))
+    LogPosition = str(os.path.dirname(os.path.dirname(__file__))) + '/LOGS/Calls/' + CallFolder + '/'
+except:
+    LogPosition = None
+
+
+# New Function creates a new folder where call data is saved to
 def New():
+    global LogPosition
     LogPosition = str(os.path.dirname(os.path.dirname(__file__))) + '/LOGS/Calls/' + str(time.time())
-    print(LogPosition)
     try:
         os.makedirs(LogPosition)
     except:
         print("Folder already exists")
+    global CallFolder
+    CallFolder = LogPosition
 
+
+# Call will return a json data structure from the ebay finding api with items in it that fit the inputs
+# Success: Success returns json data structure
+# Failure: Ran out of connection attempts : None
 def Call(Keywords,AuctionOnly,Page,MaxPrice,MinPrice,Present):
-    Retries = 5
-    SleepTime = 1
+    Retries = 5  # Number of call attempts before giving up
+    SleepTime = 1  # Time between failed calls (doubles each time)
 
-    CallFolder = str(max([float(s.replace('.txt', '')) for s in os.listdir(str(os.path.dirname(os.path.dirname(__file__))) + '/LOGS/Calls/')]))
+    global LogPosition # If no call folder exists then create a new one
+    if LogPosition == None: New()
 
+    # Database where number of call are recorded
     dbCallCount = TinyDB(os.path.dirname(os.path.dirname(__file__)) + "/DataBase/CallCount")
     Search = Query()
 
-    try:
+    try:  # Get previous call time from data base
         CallTime = dbCallCount.table("_default").all()[len(dbCallCount.table("_default").all())-1]['Time']
-    except:
+    except:  # Unable to get call time from data base
         CurrentTime = int(time.mktime(datetime.now().timetuple()))
-        dbCallCount.insert({'Calls': 0, 'Time': CurrentTime})
+        dbCallCount.insert({'Calls': 0, 'Time': CurrentTime})  # Add call time to data base
         CallTime = CurrentTime
 
-    CurrentDateTime = int(time.mktime(datetime.now().timetuple()))
+    CurrentDateTime = int(time.mktime(datetime.now().timetuple()))  # Current time
+    # Time when calls reset
     NewCallingPerioud = int(time.mktime(datetime.utcfromtimestamp(int(CallTime) + 60*60*16 -60*5).replace(hour = 1).replace(minute=5).replace(second=0).replace(microsecond=0).timetuple()))
 
-    if(CurrentDateTime > NewCallingPerioud):
+    if(CurrentDateTime > NewCallingPerioud):  # If we have passed time when calls reset then were in a new call region
         print("New Time Region")
         CurrentTime = int(time.mktime(datetime.now().timetuple()))
-        dbCallCount.insert({'Calls': 0, 'Time': CurrentTime})
+        dbCallCount.insert({'Calls': 0, 'Time': CurrentTime})  # Insert new entry with no calls
         CallTime = CurrentTime
 
     # Note calls reset 1am mtd, or 8am utc
 
-    if Present == 1:
-        APICALL = 'https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords' \
-                  '&SERVICE-VERSION=1.0.0' \
-                  '&SECURITY-APPNAME=' + SecurityAppName +  \
-                  '&RESPONSE-DATA-FORMAT=JSON' \
-                  '&REST-PAYLOAD' \
-                  '&keywords=' + Keywords + \
-                  '&paginationInput.pageNumber=' + str(Page)
-    else:
-        APICALL = 'https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems' \
-                  '&SERVICE-VERSION=1.0.0' \
-                  '&SECURITY-APPNAME=' + SecurityAppName +  \
-                  '&RESPONSE-DATA-FORMAT=JSON' \
-                  '&REST-PAYLOAD' \
-                  '&keywords=' + Keywords + \
-                  '&paginationInput.pageNumber=' + str(Page)
+    if Present == 1:  # Find current items
+        OperationName = 'findItemsByKeywords'
+    else:  # Find completed items
+        OperationName = "findCompletedItems"
+
+    APICALL = 'https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=' + OperationName + \
+              '&SERVICE-VERSION=1.0.0' \
+              '&SECURITY-APPNAME=' + SecurityAppName +  \
+              '&RESPONSE-DATA-FORMAT=JSON' \
+              '&REST-PAYLOAD' \
+              '&keywords=' + Keywords + \
+              '&paginationInput.pageNumber=' + str(Page)
 
     ItemFilterCount = 0
     APICALL = APICALL + \
