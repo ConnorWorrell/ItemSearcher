f06b7d479003d4ca02f484d2a03cfa539495d81b
kworrell kworrell <chemicalcoal1@gmail.com>
2020-01-25 22:39:16 -0700

    Added initial comments to databasecustom.py

--- Functions/DataBaseCustom.py
+++ Functions/DataBaseCustom.py
@@ -5,7 +5,7 @@ import atexit
 UPCDataBasePath = os.path.dirname(os.path.dirname(__file__)) + "/DataBase/LinkToUPC"
 ShippingDataBasePath = os.path.dirname(os.path.dirname(__file__)) + "/DataBase/LinkToShipping"
 AvgPriceDataBasePath = os.path.dirname(os.path.dirname(__file__)) + "/DataBase/LinkToAvgPrice"
-ErrorsDataBasePath = os.path.dirname(os.path.dirname(__file__)) + "/Logs/Errorss"
+ErrorsDataBasePath = os.path.dirname(os.path.dirname(__file__)) + "/Logs/Errors"
 DisplayDataDataBasePath = os.path.dirname(os.path.dirname(__file__)) + "/DataBase/DisplayData.txt"
 CallCountDataBasePath = os.path.dirname(os.path.dirname(__file__)) + "/DataBase/CallCount"
 
@@ -16,24 +16,26 @@ ErrorsDataBase = {}
 DisplayDataDataBase = {}
 CallCountDataBase = {}
 
-DatabaseWritesBeforePush = 100
+DatabaseWritesBeforePush = 100  # Writes between saving files
 DataBaseWrites = {}
 
+# Sets up proper variables for use in database stuff
 def Startup():
     global UPCDataBase, ShippingDataBase, AvgPriceDataBase, ErrorsDataBase, DisplayDataDataBase, CallCountDataBase,DataBaseWrites,InitialDataBaseSizes
 
     def Start(Path,DB):
-        DataBaseWrites[Path] = 0
-        if (os.path.exists(Path)):
+        DataBaseWrites[Path] = 0  # Reset writes between saving files count
+        if (os.path.exists(Path)):  # Check to see if database exists, if it does then load it
             with open(Path, 'r') as in_file:
                 DB = json.load(in_file)
-            return DB
-        else:
+            return DB  # Pass database back
+        else:  # Database dosen't exist then create database
             DB["_default"] = {}
             with open(Path, 'w') as out_file:
                 json.dump(DB, out_file)
-            return DB
+            return DB  # Pass database back
 
+    # Load databases
     UPCDataBase = Start(UPCDataBasePath,UPCDataBase)
     ShippingDataBase = Start(ShippingDataBasePath, ShippingDataBase)
     AvgPriceDataBase = Start(AvgPriceDataBasePath, AvgPriceDataBase)
@@ -41,13 +43,16 @@ def Startup():
     DisplayDataDataBase = Start(DisplayDataDataBasePath, DisplayDataDataBase)
     CallCountDataBase = Start(CallCountDataBasePath, CallCountDataBase)
 
-    # print(len(UPCDataBase["_default"].keys()))
+    # Record initial database size, used to compare to end data base size at end of run
     InitialDataBaseSizes = [len(UPCDataBase["_default"].keys()),len(ShippingDataBase["_default"].keys()),len(AvgPriceDataBase["_default"].keys()),len(ErrorsDataBase["_default"].keys()),len(CallCountDataBase["_default"].keys())]
 
     print("Loaded")
 
+# Called at the end of run, saves all database related things
 def End():
     global UPCDataBase, ShippingDataBase, AvgPriceDataBase, ErrorsDataBase, DisplayDataDataBase, CallCountDataBase,DataBaseWrites,InitialDataBaseSizes
+
+    # Save databases, note that commented out databases are not currently being used and saving them would overwrite the actual TinyDB database
     PushUPCDataBase()
     PushAvgDataBase()
     # PushCallDataBase()
@@ -55,23 +60,24 @@ def End():
     # PushErrorsDataBase()
     PushShippingDataBase()
 
+    # Record size of database at end of run
     FinalDataBaseSizes = [len(UPCDataBase["_default"].keys()),len(ShippingDataBase["_default"].keys()),len(AvgPriceDataBase["_default"].keys()),len(ErrorsDataBase["_default"].keys()),len(CallCountDataBase["_default"].keys())]
 
+    # Print difference between size of databases between beginning and end of run
     for a in range(len(InitialDataBaseSizes)):
         print(FinalDataBaseSizes[a]-InitialDataBaseSizes[a])
 
     print("Finished")
 
 
-
+# Dump saves database DB to file located at Path
 def Dump(Path,DB):
-    # print("Dumping to " + str(Path) + "  |  " + str(DB))
     with open(Path, 'w') as out_file:
         json.dump(DB, out_file)
 
+# Push Database saves specific data base
 def PushUPCDataBase():
     global UPCDataBase
-    # print("Push")
     Dump(UPCDataBasePath,UPCDataBase)
 
 def PushShippingDataBase():
@@ -94,18 +100,22 @@ def PushCallDataBase():
     global CallCountDataBase
     Dump(CallCountDataBasePath, CallCountDataBase)
 
+
+# Add adds an item to a database
 def Add(Path,DB,Value):
+    # In database {"1":data,"3":data,"NextNumber":Value} NextNumber has a value 1 higher than the current highest number
     nextNumber = (max([int(a) for a in DB["_default"].keys()]+[0])+1)
     DB["_default"][str(nextNumber)] = Value
 
+    # If it is time to save database to file then save to file
     DataBaseWrites[Path] = DataBaseWrites[Path] + 1
     if(DataBaseWrites[Path] >= DatabaseWritesBeforePush):
-        DataBaseWrites[Path] = 0
-        Dump(Path,DB)
+        DataBaseWrites[Path] = 0  # Reset counter
+        Dump(Path,DB)  # Save to file
 
+# Add Value to Database for each database
 def AddUPCDataBase(Value):
     global UPCDataBase
-    # print("Add")
     Add(UPCDataBasePath,UPCDataBase,Value)
 
 def AddShippingDataBase(Value):
@@ -130,25 +140,31 @@ def AddCallDataBase(Value):
     Add(CallCountDataBasePath, CallCountDataBase,Value)
 
 
+# Find returns all items in the data base matching the Search Item dict
+# If searching for item with Name = Bob and Value = Friendly then: SearchItem = {"Name":"Bob","Value":"Friendly"}
 def Find(Path,DB,SearchItem):
-    SearchValues = DB['_default']
-    Results = []
+    SearchValues = DB['_default']  # Informaion from database
+    Results = []  # Initilize place for results to end up
 
+    # Split keys and values apart to [[Name,Value],[Bob,Friendly]]
     SearchFor = [[a for a in SearchItem.keys()], [b for b in SearchItem.values()]]
-    for ItemIndex in SearchValues:
+    for ItemIndex in SearchValues:  # Go through all values in database
+        for SearchIndex in range(len(SearchFor[0])):  # Go through all key-value combos in SearchFor
+            SearchingForKey = SearchFor[0][SearchIndex]  # Grab key to compare
+            SearchingForValue = SearchFor[1][SearchIndex]  # Grab value to compare
 
-        for SearchIndex in range(len(SearchFor[0])):
-            SearchingForKey = SearchFor[0][SearchIndex]
-            SearchingForValue = SearchFor[1][SearchIndex]
+            # If key-value combo is not in ItemIndex, move to next item
             if(SearchingForKey not in SearchValues[ItemIndex].keys() or SearchValues[ItemIndex][SearchingForKey] != SearchingForValue): break
-            elif(SearchIndex == len(SearchFor[0])-1):
-                Results.append(SearchValues[ItemIndex])
+            elif(SearchIndex == len(SearchFor[0])-1):  # Key value combo in and run out of key value combos
+                Results.append(SearchValues[ItemIndex])  # This is a hit
+
+            # Key-Value combo in search, but there are still more key-value combos to check before it can be called a result
 
     return Results
 
+# Find Value in database for each database
 def FindUPCDataBase(Value):
     global UPCDataBase
-    # print("Find")
     return Find(UPCDataBasePath,UPCDataBase,Value)
 
 def FindShippingDataBase(Value):
@@ -172,16 +188,19 @@ def FindCallDataBase(Value):
     return Find(CallCountDataBasePath, CallCountDataBase,Value)
 
 
+# Clear removes everying from database and initilizes default state {"_default"{}}
 def Clear(Path,DB):
-    DB = {}
+    DB = {}  # Create default state
     DB["_default"] = {}
-    with open(Path, 'w') as out_file:
-        json.dump(DB, out_file)
-    return DB
 
+    with open(Path, 'w') as out_file:  # Save default state to file
+        json.dump(DB, out_file)
+    return DB  # Pass default state to global database
+
+
+# Clear database for all databases
 def ClearUPCDataBase():
     global UPCDataBase
-    # print("Clear")
     UPCDataBase = Clear(UPCDataBasePath,UPCDataBase)
 
 def ClearShippingDataBase():
@@ -205,40 +224,45 @@ def ClearCallDataBase():
     CallCountDataBase = Clear(CallCountDataBasePath, CallCountDataBase)
 
 
+# Remove first item from data base matching some value, if you want to remove the first thing with name bob, then
+# RemoveItem = {"Name":"bob"}
 def Remove(Path,DB,RemoveItem):
-    ItemPop = None
+    ItemPop = None  # Initilize value to see if item remove successfull
     print(len(DB["_default"].keys()))
 
     SearchValues = DB['_default']
-    SearchFor = [[a for a in RemoveItem.keys()], [b for b in RemoveItem.values()]]
-    for ItemIndex in SearchValues:
-
-        for SearchIndex in range(len(SearchFor[0])):
+    SearchFor = [[a for a in RemoveItem.keys()], [b for b in RemoveItem.values()]]  # Key-Value comparision combo
+    for ItemIndex in SearchValues:  # Go through all items in database
+        for SearchIndex in range(len(SearchFor[0])):  # Go through all key-value comboes
             SearchingForKey = SearchFor[0][SearchIndex]
             SearchingForValue = SearchFor[1][SearchIndex]
+
+            # If key-value dosen't match then move onto next Item in database
             if(SearchingForKey not in SearchValues[ItemIndex].keys() or SearchValues[ItemIndex][SearchingForKey] != SearchingForValue): break
+            # If key-value does match and no more key-value combos to check then item to remove was found
             elif(SearchIndex == len(SearchFor[0])-1):
-                ItemPop = ItemIndex
+                ItemPop = ItemIndex  # Item to removes position in database
+            # Key value found but there are still more key values to check
 
-    if(ItemPop != None):
+    if(ItemPop != None):  # If Item was found to remove
         print("Success")
-        DB["_default"].pop(ItemPop)
-        Success = True
+        DB["_default"].pop(ItemPop)  # Remove Item
+        Success = True  # Value returned
     else:
-        Success = False
+        Success = False  # If no item was found return false
 
+    # Check if database needs to be written to
     global DataBaseWrites
     DataBaseWrites[Path] = DataBaseWrites[Path] + 1
     if (DataBaseWrites[Path] >= DatabaseWritesBeforePush):
         DataBaseWrites[Path] = 0
         Dump(Path, DB)
 
-    print(len(DB["_default"].keys()))
-    return Success,DB
+    return Success,DB  # Return True/False if item pop successfull and database with item removed
 
+# Remove for all databases
 def RemoveUPCDataBase(RemoveObject):
     global UPCDataBase
-    # print("Remove")
     Success, UPCDataBase = Remove(UPCDataBasePath,UPCDataBase,RemoveObject)
 
 def RemoveShippingDataBase(RemoveObject):
@@ -267,84 +291,92 @@ def RemoveCallDataBase(RemoveObject):
     return Success
 
 
+# All returns the entire database structure
 def All(Path,DB):
     return DB["_default"]
 
-
 def AllUPCDataBase():
-    print("All")
     global UPCDataBase
-    UPCDataBase = All(UPCDataBasePath,UPCDataBase)
+    return All(UPCDataBasePath,UPCDataBase)
 
 def AllShippingDataBase():
     global ShippingDataBase
-    ShippingDataBase = All(ShippingDataBasePath, ShippingDataBase)
+    return All(ShippingDataBasePath, ShippingDataBase)
 
 def AllAvgDataBase():
     global AvgPriceDataBase
-    AvgPriceDataBase = All(AvgPriceDataBasePath, AvgPriceDataBase)
+    return All(AvgPriceDataBasePath, AvgPriceDataBase)
 
 def AllErrorsDataBase():
     global ErrorsDataBase
-    ErrorsDataBase = All(ErrorsDataBasePath, ErrorsDataBase)
+    return All(ErrorsDataBasePath, ErrorsDataBase)
 
 def AllDisplayDataBase():
     global DisplayDataDataBase
-    DisplayDataDataBase = All(DisplayDataDataBasePath, DisplayDataDataBase)
+    return All(DisplayDataDataBasePath, DisplayDataDataBase)
 
 def AllCallDataBase():
     global CallCountDataBase
-    CallCountDataBase = All(CallCountDataBasePath, CallCountDataBase)
+    return All(CallCountDataBasePath, CallCountDataBase)
 
-def Update(Path,DB,ValueToIncriment,SearchItem):
+
+# Incriment incriments the first value that is located by searchItem
+def Incriment(Path,DB,ValueToIncriment,SearchItem):
     SearchValues = DB['_default']
-    Results = []
 
-    SearchFor = [[a for a in SearchItem.keys()], [b for b in SearchItem.values()]]
-    for ItemIndex in SearchValues:
-
-        for SearchIndex in range(len(SearchFor[0])):
+    SearchFor = [[a for a in SearchItem.keys()], [b for b in SearchItem.values()]]  # Key-Value Combo
+    for ItemIndex in SearchValues:  # Searches all items in database
+        for SearchIndex in range(len(SearchFor[0])):  # Searches all key-value combos
             SearchingForKey = SearchFor[0][SearchIndex]
             SearchingForValue = SearchFor[1][SearchIndex]
+
+            # If key-value not found in current searching item move onto next item
             if (SearchingForKey not in SearchValues[ItemIndex].keys() or SearchValues[ItemIndex][
                 SearchingForKey] != SearchingForValue):
                 break
+            # Found value to incriment
             elif (SearchIndex == len(SearchFor[0]) - 1):
+                # Incriment value
                 SearchValues[ItemIndex][ValueToIncriment] = int(SearchValues[ItemIndex][ValueToIncriment]) + 1
+            # key-value found but still have more key-value combos to check
 
+    # Check if database needs to be saved
     DataBaseWrites[Path] = DataBaseWrites[Path] + 1
     if (DataBaseWrites[Path] >= DatabaseWritesBeforePush):
         DataBaseWrites[Path] = 0
         Dump(Path, DB)
 
-def UpdateUPCDataBase(ValueToIncriment,SearchItem):
+# Incriment for all databases
+def IncrimentUPCDataBase(ValueToIncriment,SearchItem):
     global UPCDataBase
-    UPCDataBase = Update(UPCDataBasePath,UPCDataBase,ValueToIncriment,SearchItem)
+    UPCDataBase = Incriment(UPCDataBasePath,UPCDataBase,ValueToIncriment,SearchItem)
 
-def UpdateShippingDataBase(ValueToIncriment,SearchItem):
+def IncrimentShippingDataBase(ValueToIncriment,SearchItem):
     global ShippingDataBase
-    ShippingDataBase = Update(ShippingDataBasePath, ShippingDataBase,ValueToIncriment,SearchItem)
+    ShippingDataBase = Incriment(ShippingDataBasePath, ShippingDataBase,ValueToIncriment,SearchItem)
 
-def UpdateAvgDataBase(ValueToIncriment,SearchItem):
+def IncrimentAvgDataBase(ValueToIncriment,SearchItem):
     global AvgPriceDataBase
-    AvgPriceDataBase = Update(AvgPriceDataBasePath, AvgPriceDataBase,ValueToIncriment,SearchItem)
+    AvgPriceDataBase = Incriment(AvgPriceDataBasePath, AvgPriceDataBase,ValueToIncriment,SearchItem)
 
-def UpdateErrorsDataBase(ValueToIncriment,SearchItem):
+def IncrimentErrorsDataBase(ValueToIncriment,SearchItem):
     global ErrorsDataBase
-    ErrorsDataBase = Update(ErrorsDataBasePath, ErrorsDataBase,ValueToIncriment,SearchItem)
+    ErrorsDataBase = Incriment(ErrorsDataBasePath, ErrorsDataBase,ValueToIncriment,SearchItem)
 
-def UpdateDisplayDataBase(ValueToIncriment,SearchItem):
+def IncrimentDisplayDataBase(ValueToIncriment,SearchItem):
     global DisplayDataDataBase
-    DisplayDataDataBase = Update(DisplayDataDataBasePath, DisplayDataDataBase,ValueToIncriment,SearchItem)
+    DisplayDataDataBase = Incriment(DisplayDataDataBasePath, DisplayDataDataBase,ValueToIncriment,SearchItem)
 
-def UpdateCallDataBase(ValueToIncriment,SearchItem):
+def IncrimentCallDataBase(ValueToIncriment,SearchItem):
     global CallCountDataBase
-    CallCountDataBase = Update(CallCountDataBasePath, CallCountDataBase,ValueToIncriment,SearchItem)
+    CallCountDataBase = Incriment(CallCountDataBasePath, CallCountDataBase,ValueToIncriment,SearchItem)
 
+# When program stops save all data
 @atexit.register
 def OnExit():
-    End()
+    End()  # Saves all data
 
+# Tests database write speed
 def Test():
     ClearErrorsDataBase()
 
@@ -359,7 +391,4 @@ def Test():
     for i in range(100):
         print(FindErrorsDataBase({"Name":str(i*20)}))
 
-Startup()
-# ClearErrorsDataBase()
-# AddErrorsDataBase({"Test":"hi3"})
-# RemoveErrorsDataBase({"Test":"hi3"})
+Startup()  # Initilize database
